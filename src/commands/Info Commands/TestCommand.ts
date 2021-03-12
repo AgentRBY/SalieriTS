e0640bcacbd2f0496321f41a70af86f48ba7fa42
Agent_RBY_ <mrget112@gmail.com>
2021-03-12 22:45:05 +0300

    chore(test.ts): update test command

--- src/commands/Info Commands/TestCommand.ts
+++ src/commands/Info Commands/TestCommand.ts
@@ -1,4 +1,5 @@
 import { Example, RunFunction } from "./../../interfaces/ICommand";
+import { PermissionString } from "discord.js";
 
 export const name: string = "test";
 export const category: string = "info";
@@ -11,6 +12,9 @@ export const examples: Array<Example> = [
     description: "",
   },
 ];
+export const memberPermissions: Array<PermissionString> = [];
+export const botPermissions: Array<PermissionString> = [];
+export const ownerOnly: boolean = true;
 export const run: RunFunction = async (client, message, args) => {
   client.logger.info("test");
 };
