3e27386d832ef89cd837dbf3ce650c066ef6d4a6
Agent_RBY_ <mrget112@gmail.com>
2021-03-14 17:54:40 +0300

    feat(BotConfig): add some emoji

--- src/static/BotConfig.ts
+++ src/static/BotConfig.ts
@@ -1,9 +1,14 @@
 export const emojis = {
-  Yes: "<:Yes:820579428933042217>",
-  No: "<:No:820576440260624384>",
+  Yes: "<:YesEmoji:820579428933042217>",
+  No: "<:NoEmoji:820576440260624384>",
+  Add: "<:AddEmoji:820629887701352499>",
+  Remove: "<:RemoveEmoji:820629887697682449>",
+  Settings: "<:SettingsEmoji:820629887886163968>",
+  Warn: "<:WarnEmoji:820629887722586153>",
 };
 export const colors = {
   Green: "#2ECC71",
   Red: "#E74C3C",
   Orange: "#E67E22",
+  Gray: "#485761",
 };
