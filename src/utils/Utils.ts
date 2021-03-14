4094ec77381e0d2ab8dc4df18fda5dcddcf86141
Agent_RBY_ <mrget112@gmail.com>
2021-03-14 17:57:51 +0300

    feat(Utils): add validateBoolean and embedUtils

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,5 +1,5 @@
 import { Bot } from "../client/Client";
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, Snowflake } from "discord.js";
 import { colors, emojis } from "./../static/BotConfig";
 
 class Utils {
@@ -9,12 +9,53 @@ class Utils {
     this.client.logger.info("Utils has been constructed");
   }
 
-  public createSuccessEmbed(description: String): MessageEmbed {
-    return new MessageEmbed().setDescription(`${emojis.Yes} ${description}`).setColor(colors.Green);
-  }
-  public createErrorEmbed(description: String): MessageEmbed {
-    return new MessageEmbed().setDescription(`${emojis.No} ${description}`).setColor(colors.Red);
+  public validateBoolean(value: string): boolean | undefined {
+    if (/^(?:y|yes|true|t|1|on|enable)$/i.test(value)) {
+      return true;
+    }
+
+    if (/^(?:n|no|false|f|0|off|disable)$/i.test(value)) {
+      return false;
+    }
+
+    return undefined;
   }
 }
 
-export { Utils };
+class EmbedUtils {
+  private client: Bot;
+  constructor(client: Bot) {
+    this.client = client;
+    this.client.logger.info("Embed Utils has been constructed");
+  }
+
+  public createSuccessEmbed(description: String): MessageEmbed {
+    return new MessageEmbed()
+      .setDescription(`${emojis.Yes}  ${description}`)
+      .setColor(colors.Green);
+  }
+  public createErrorEmbed(description: String): MessageEmbed {
+    return new MessageEmbed()
+      .setDescription(`${emojis.No}  ${description}`)
+      .setColor(colors.Red);
+  }
+  public createSettingsEmbed(description: String): MessageEmbed {
+    return new MessageEmbed()
+      .setDescription(`${emojis.Settings}  ${description}`)
+      .setColor(colors.Gray);
+  }
+}
+
+class UserUtils {
+  private client: Bot;
+  constructor(client: Bot) {
+    this.client = client;
+    this.client.logger.info("Embed Utils has been constructed");
+  }
+
+  public getUserById(id: Snowflake) {
+    return this.client.users.cache.get(id);
+  }
+}
+
+export { EmbedUtils, Utils };
