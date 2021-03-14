0881dda5c3acc07090ef8397fcfdbf5314f013cd
Agent_RBY_ <mrget112@gmail.com>
2021-03-14 13:16:41 +0300

    feat(BotConfig): add emojis and colors

---
+++ src/static/BotConfig.ts
@@ -0,0 +1,9 @@
+export const emojis = {
+  Yes: "<:Yes:820579428933042217>",
+  No: "<:No:820576440260624384>",
+};
+export const colors = {
+  Green: "#2ECC71",
+  Red: "#E74C3C",
+  Orange: "#E67E22",
+};
